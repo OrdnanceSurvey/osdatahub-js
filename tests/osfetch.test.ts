e97788ad422324fad0a022c9292cce25edda7acc
Dan Hirst <93535587+dchirst@users.noreply.github.com>
2022-09-29 15:37:15 +0100

    Added osfetch.test.ts

---
+++ tests/osfetch.test.ts
@@ -0,0 +1,8 @@
+import {describe, expect, test} from '@jest/globals';
+import { osfetch } from '../src/index'
+
+describe("osfetch", () => {
+    test("My First Test", () => {
+        expect(5 + 5).toEqual(10)
+    });
+});
--- tests/sanitise.test.js
+++
@@ -1,6 +0,0 @@
-import { osfetch } from '../src/index.mjs'
-
-test("Runs sanitise without api key and expects error", () => {
-
-    expect(osfetch.names({})).toThrow();
-});
