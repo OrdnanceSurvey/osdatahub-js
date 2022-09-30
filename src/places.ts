2eae1381d10a3e23eadbf54ba3beff49ebee54d1
Josh <Josh.Pooley@os.uk>
2022-09-30 11:43:35 +0100

    mob-1

--- src/places.ts
+++ src/places.ts
@@ -5,14 +5,14 @@ import { request } from './utils/request.js'
 import { geojson } from './utils/geojson.js'
 
 export {
-    handlePlaces
+    places
 }
 
-async function handlePlaces(params) {
+async function handlePlaces(apiKey: string, params) {
 
     let config = {
         url: '',
-        key: params.apiKey,
+        key: apiKey,
         body: '',
         method: 'get',
         paging: {
@@ -26,47 +26,47 @@ async function handlePlaces(params) {
 
     let rectifiedCoords
 
-    switch (params.findBy[0]) {
+    // switch (params.findBy[0]) {
 
-        case 'polygon':
-            config.url = `https://api.os.uk/search/places/v1/polygon?srs=WGS84`
-            config.method = 'post'
-            config.body = JSON.stringify(geojson.from(params.findBy[1]))
-            break
+    //     case 'polygon':
+    //         config.url = `https://api.os.uk/search/places/v1/polygon?srs=WGS84`
+    //         config.method = 'post'
+    //         config.body = JSON.stringify(geojson.from(params.findBy[1]))
+    //         break
 
-        case 'radius':
-            rectifiedCoords = coords.swivel(params.findBy[1])
-            config.url = `https://api.os.uk/search/places/v1/radius?srs=WGS84&point=${rectifiedCoords}&radius=${params.findBy[2]}`
-            break
+    //     case 'radius':
+    //         rectifiedCoords = coords.swivel(params.findBy[1])
+    //         config.url = `https://api.os.uk/search/places/v1/radius?srs=WGS84&point=${rectifiedCoords}&radius=${params.findBy[2]}`
+    //         break
 
-        case 'bbox':
-            rectifiedCoords = coords.swivel(params.findBy[1])
-            config.url = `https://api.os.uk/search/places/v1/bbox?srs=WGS84&bbox=${rectifiedCoords}`
-            break
+    //     case 'bbox':
+    //         rectifiedCoords = coords.swivel(params.findBy[1])
+    //         config.url = `https://api.os.uk/search/places/v1/bbox?srs=WGS84&bbox=${rectifiedCoords}`
+    //         break
 
-        case 'nearest':
-            rectifiedCoords = coords.swivel(params.findBy[1])
-            config.url = `https://api.os.uk/search/places/v1/nearest?srs=WGS84&point=${rectifiedCoords}`
-            config.paging.enabled = false
-            break
+    //     case 'nearest':
+    //         rectifiedCoords = coords.swivel(params.findBy[1])
+    //         config.url = `https://api.os.uk/search/places/v1/nearest?srs=WGS84&point=${rectifiedCoords}`
+    //         config.paging.enabled = false
+    //         break
         
-        case 'uprn':
-            config.url = `https://api.os.uk/search/places/v1/uprn?output_srs=WGS84&uprn=${params.findBy[1]}`
-            config.paging.enabled = false
-            break
+    //     case 'uprn':
+    //         config.url = `https://api.os.uk/search/places/v1/uprn?output_srs=WGS84&uprn=${params.findBy[1]}`
+    //         config.paging.enabled = false
+    //         break
 
-        case 'postcode':
-            config.url = `https://api.os.uk/search/places/v1/postcode?output_srs=WGS84&postcode=${params.findBy[1]}`
-            break
+    //     case 'postcode':
+    //         config.url = `https://api.os.uk/search/places/v1/postcode?output_srs=WGS84&postcode=${params.findBy[1]}`
+    //         break
 
-        case 'find':
-            config.url = `https://api.os.uk/search/places/v1/find?output_srs=WGS84&query=${params.findBy[1]}`
-            break
+    //     case 'find':
+    //         config.url = `https://api.os.uk/search/places/v1/find?output_srs=WGS84&query=${params.findBy[1]}`
+    //         break
 
-        default:
-            throw new Error('Invalid findBy type supplied. Aborting.')
+    //     default:
+    //         throw new Error('Invalid findBy type supplied. Aborting.')
 
-    }
+    // }
 
     let responseObject = await request(config)
 
@@ -74,4 +74,13 @@ async function handlePlaces(params) {
 
     return responseObjectGeoJSON
 
+}
+
+
+const places = {
+    polygon: (apiKey: string, polygon, params) {
+        config.url = `https://api.os.uk/search/places/v1/polygon?srs=WGS84`
+        config.method = 'post'
+        config.body = JSON.stringify(geojson.from(polygon))
+    }
 }
