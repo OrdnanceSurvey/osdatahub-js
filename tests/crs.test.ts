53de9aebcba5ad5bde1b64a3077f51fe3d72afde
Josh Pooley <joshpooley93@gmail.com>
2022-10-31 07:58:39 +0000

    test: add error test to getCRS

--- tests/crs.test.ts
+++ tests/crs.test.ts
@@ -13,15 +13,25 @@ async function testError(callback: Function): Promise<any> {
 }
 
 describe("Get valid CRS", () => {
-  test("getCRS with string passes", async () => {
+  test("getCRS with string passes", () => {
     expect(getCRS("epsg:27700")).toBe("epsg:27700");
   });
 
-  test("getCRS with number passes", async () => {
+  test("getCRS with number passes", () => {
     expect(getCRS(4326)).toBe("epsg:4326");
   });
 
-  test("getCRS with uppercase string passes", async () => {
+  test("getCRS with uppercase string passes", () => {
     expect(getCRS("WGS84")).toBe("wgs84");
+  });
+
+  test("getCRS fails for unsupported epsg", async () => {
+    const error = await testError(() => getCRS(34599));
+    expect(error).toEqual(new Error("Unrecognised CRS"));
+  });
+
+  test("getCRS fails for unrecognisable string", async () => {
+    const error = await testError(() => getCRS("british national grid"));
+    expect(error).toEqual(new Error("Unrecognised CRS"));
   });
 });
