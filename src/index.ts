cd29ac159aa6fbb4d908b970d67cf6bc865c58c9
JEPooley <JEPooley@users.noreply.github.com>
2022-11-02 09:52:46 +0000

    Prettified Code!

--- src/index.ts
+++ src/index.ts
@@ -19,6 +19,6 @@
 import { names as namesAPI } from "./names.js";
 import { places as placesAPI } from "./places.js";
 import { ngd } from "./ngd.js";
-import 'isomorphic-fetch';
+import "isomorphic-fetch";
 
 export { namesAPI, placesAPI, ngd };
