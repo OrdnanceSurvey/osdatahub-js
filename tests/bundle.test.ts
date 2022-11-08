0b6605119196383711e638d5b0d62e42984b0c23
JEPooley <JEPooley@users.noreply.github.com>
2022-11-08 11:35:12 +0000

    Prettified Code!

--- tests/bundle.test.ts
+++ tests/bundle.test.ts
@@ -1,17 +1,16 @@
-import * as osdatahub from "../dist/bundle.min.js"
+import * as osdatahub from "../dist/bundle.min.js";
 import { describe, expect, test } from "@jest/globals";
 
-
 describe("All modules are available", () => {
-    test("Places", () => {
-      expect(osdatahub.placesAPI).toBeTruthy()
-    });
+  test("Places", () => {
+    expect(osdatahub.placesAPI).toBeTruthy();
+  });
 
-    test("Names", () => {
-        expect(osdatahub.namesAPI).toBeTruthy()
-      });
+  test("Names", () => {
+    expect(osdatahub.namesAPI).toBeTruthy();
+  });
 
-      test("NGD", () => {
-        expect(osdatahub.ngd).toBeTruthy()
-      });
-})
+  test("NGD", () => {
+    expect(osdatahub.ngd).toBeTruthy();
+  });
+});
